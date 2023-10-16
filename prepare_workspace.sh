850556b45f9f52a2bc3e8a4c6b40f614b14d25d5
Addie Tsai-Yun, Chung <addiechung.tyc@gmail.com>
2023-10-16 21:16:09 +0800

    feat: add students in prepare script

--- prepare_workspace.sh
+++ prepare_workspace.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-declare -a students=() 
+declare -a students=(ramy vincent josh jolie yuna sandy nick ann tim jason joe martin lando jaren) 
 
-# dir=workspace
-# rm -r ./${dir}
-# mkdir ${dir}
-# cd ${dir}
+dir=workspace
+rm -r ./${dir}
+mkdir ${dir}
+cd ${dir}
 
-# for i in "${students[@]}"
-# do
-#    :
-#    echo add folder: students/${i} 
-#    mkdir ${i}
-#    touch ${i}/README.md
-# done
+for i in "${students[@]}"
+do
+   :
+   echo add folder: students/${i} 
+   mkdir ${i}
+   touch ${i}/README.md
+done
 
 for i in "${students[@]}"
 do
