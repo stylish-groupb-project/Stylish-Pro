f65c74542523894998ea906180871e9112813774
Lu-weiting <runeeld23@gmail.com>
2023-10-19 18:49:55 +0800

    modify swagger set

--- workspace/tim/stylish/app.js
+++ workspace/tim/stylish/app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-
+const cors = require('cors');
 const product_router = require('./Router/product_router');
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/1.0/products',product_router);
 
 app.use('/static',express.static(__dirname+'/static'));
 app.use(
-    '/apidoc',
+    '/doc',
     swaggerUi.serve,
     swaggerUi.setup(swaggerDocument)
 );
