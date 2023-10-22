2b45a6edd4dc5810cdc95076af6023f67c557457
Lu-weiting <runeeld23@gmail.com>
2023-10-22 22:34:22 +0800

    add errorMsg

--- workspace/tim/stylish/Application/Features/Product/Commands/createProduct/createProductHandler.js
+++ workspace/tim/stylish/Application/Features/Product/Commands/createProduct/createProductHandler.js
@@ -5,6 +5,7 @@ const sizeService = require('../../../../../Service/sizeService');
 const variantService = require('../../../../../Service/variantService');
 const imageService = require('../../../../../Service/imageService');
 const createProductResponse = require('./createProductRes');
+const errorMsg = require('../../../../../utils/error');
 module.exports = {
     handle: async (res, data, uploadedPictures) => {
         //init
@@ -17,6 +18,7 @@ module.exports = {
         console.log(mainImage);
         const otherImages = uploadedPictures['other_images'];
         const mainImageUrl = await tool.uploadToS3(mainImage);
+        if(otherImages.length === 0 || !mainImage) return errorMsg.inputEmpty(res);
         const otherImageUrls = await Promise.all(otherImages.map(tool.uploadToS3));
         console.log("檔案全部上傳到S3成功");
 
@@ -27,7 +29,7 @@ module.exports = {
         await sizeService.addSizeIntoProduct(res,data_json.sizes,productId);
         await variantService.addVariantIntoProduct(res,data_json.variants,productId);
         await imageService.addImageIntoProduct(res,otherImageUrls,productId);
-        
+
         response = await createProductResponse.customize(result);
         return response;
     }
