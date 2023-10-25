fb2fefd7977aec24be13bf4ed9e644d3fe218f70
Lu-weiting <runeeld23@gmail.com>
2023-10-25 21:54:41 +0800

    finish signin api

--- workspace/tim/stylish/Controller/user_controller.js
+++ workspace/tim/stylish/Controller/user_controller.js
@@ -1,10 +1,21 @@
 
 const userSignUpHandler = require('../Application/Features/UserInfo/Commands/UserSignUp/userSignUpHandler');
+const userSignInHandler = require('../Application/Features/UserInfo/Commands/UserSignIn/userSignInHandler');
+
 module.exports = {
     signUp: async(req,res)=>{
         try {
             const { name , email , password} = req.body;
             const response=await userSignUpHandler.handle(res, name , email , password);
+            res.status(200).json(response);
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    signIn: async(req,res)=>{
+        try {
+            const { provider , email , password } = req.body;
+            const response=await userSignInHandler.handle(res, provider , email , password);
             res.status(200).json(response);
         } catch (error) {
             console.log(error)
--- workspace/tim/stylish/Service/userService.js
+++ workspace/tim/stylish/Service/userService.js
@@ -1,4 +1,4 @@
-
+// Service layer (負責關於資料庫相關的邏輯處理)
 const userRepo = require('../Repository/userRepo');
 const errorMsg = require('../utils/error');
 module.exports = {
@@ -9,5 +9,12 @@ module.exports = {
         }
         const insertResult = await userRepo.insertNewUser(res,userIfoObj);
         return insertResult;
+    },
+    signIn: async(res, email)=>{
+        const result = await userRepo.selectUserByEmail(res,email);
+        if (result.length === 0) {
+            return errorMsg.noUser(res);
+        }
+        return result;
     }
 }
