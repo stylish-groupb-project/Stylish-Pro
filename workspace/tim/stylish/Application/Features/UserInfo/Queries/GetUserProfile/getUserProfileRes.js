ac136be21464c8f4df0f1fcc1907c6d86cc5b32f
Lu-weiting <runeeld23@gmail.com>
2023-10-26 16:39:47 +0800

    modify profile api res

--- workspace/tim/stylish/Application/Features/UserInfo/Queries/GetUserProfile/getUserProfileRes.js
+++ workspace/tim/stylish/Application/Features/UserInfo/Queries/GetUserProfile/getUserProfileRes.js
@@ -3,7 +3,6 @@ module.exports = {
         const response = {
             data: {
                 user: {
-                    id: userData[0].id,
                     provider: userData[0].provider,
                     name: userData[0].name,
                     email: userData[0].email,
