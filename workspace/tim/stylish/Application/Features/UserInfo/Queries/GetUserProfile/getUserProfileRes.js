558e8944b1f8192657ddc3b63ced1163f3c6a558
Lu-weiting <runeeld23@gmail.com>
2023-10-27 15:47:24 +0800

    modify profile res

--- workspace/tim/stylish/Application/Features/UserInfo/Queries/GetUserProfile/getUserProfileRes.js
+++ workspace/tim/stylish/Application/Features/UserInfo/Queries/GetUserProfile/getUserProfileRes.js
@@ -1,13 +1,11 @@
 module.exports = {
-    customize: async(userData)=>{
+    customize: async (userData) => {
         const response = {
             data: {
-                user: {
-                    provider: userData[0].provider,
-                    name: userData[0].name,
-                    email: userData[0].email,
-                    picture: userData[0].picture
-                }
+                provider: userData[0].provider,
+                name: userData[0].name,
+                email: userData[0].email,
+                picture: userData[0].picture
             }
         };
         return response;
