51f4c28afd55eef8174373fc2d3fb3916d9e1108
Lu-weiting <runeeld23@gmail.com>
2023-11-08 14:55:50 +0800

    Fix clean some defalut in frontend container

--- workspace/tim/stylish/frontend/src/App.js
+++ workspace/tim/stylish/frontend/src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
-// import './App.css';
+
 import Header from './components/Header';
 import { Footer } from './components/Footer';
 import Main from './components/Main';
-// import { Footer } from './components/Footer';
 
 function App() {
   return (
