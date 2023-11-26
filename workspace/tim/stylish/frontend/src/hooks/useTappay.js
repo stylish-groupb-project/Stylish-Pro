1fbd03cfaf59eb63bcb9cf1dab357438163f9589
Lu-weiting <runeeld23@gmail.com>
2023-11-26 16:01:05 +0800

    Fix: try solving bug on ec2

--- workspace/tim/stylish/frontend/src/hooks/useTappay.js
+++ workspace/tim/stylish/frontend/src/hooks/useTappay.js
@@ -9,36 +9,37 @@ const VITE_TAPPAY_APPKEY = process.env.REACT_APP_VITE_TAPPAY_APPKEY;
 const VITE_TAPPAY_SERVER_TYPE = process.env.REACT_APP_VITE_TAPPAY_SERVER_TYPE;
 
 const useTappay = () => {
-  useEffect(() => {
-    TPDirect.setupSDK(VITE_TAPPAY_APPID, VITE_TAPPAY_APPKEY, VITE_TAPPAY_SERVER_TYPE);
-    TPDirect.card.setup({
-      fields: {
-        number: {
-          element: "#card-number",
-          placeholder: "**** **** **** ****",
-        },
-        expirationDate: {
-          element: document.getElementById("card-expiration-date"),
-          placeholder: "MM / YY",
-        },
-        ccv: {
-          element: "#card-ccv",
-          placeholder: "後三碼",
-        },
-      },
-      isMaskCreditCardNumber: true,
-      maskCreditCardNumberRange: {
-        beginIndex: 0,
-        endIndex: 16,
-      },
-    });
-    TPDirect.card.onUpdate((update) => {
-      const newType = update.cardType === "unknown" ? "" : update.cardType;
-      $("#cardtype").text(newType);
-    // document.getElementById("cardtype").textContent = newType;
+    console.log(`tappay: ${VITE_TAPPAY_APPID} ,${VITE_TAPPAY_APPKEY}, ${VITE_TAPPAY_SERVER_TYPE}`);
+    useEffect(() => {
+        TPDirect.setupSDK(VITE_TAPPAY_APPID, VITE_TAPPAY_APPKEY, VITE_TAPPAY_SERVER_TYPE);
+        TPDirect.card.setup({
+            fields: {
+                number: {
+                    element: "#card-number",
+                    placeholder: "**** **** **** ****",
+                },
+                expirationDate: {
+                    element: document.getElementById("card-expiration-date"),
+                    placeholder: "MM / YY",
+                },
+                ccv: {
+                    element: "#card-ccv",
+                    placeholder: "後三碼",
+                },
+            },
+            isMaskCreditCardNumber: true,
+            maskCreditCardNumberRange: {
+                beginIndex: 0,
+                endIndex: 16,
+            },
+        });
+        TPDirect.card.onUpdate((update) => {
+            const newType = update.cardType === "unknown" ? "" : update.cardType;
+            $("#cardtype").text(newType);
+            // document.getElementById("cardtype").textContent = newType;
 
-    });
-  }, []);
+        });
+    }, []);
 };
 
 export default useTappay;
