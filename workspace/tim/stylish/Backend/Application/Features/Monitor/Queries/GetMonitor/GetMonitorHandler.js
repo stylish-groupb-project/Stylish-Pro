d267cca60a2a496e8ac3e182486c87da7f22a360
Lu-weiting <runeeld23@gmail.com>
2023-12-10 00:27:50 +0800

    Fix: service prob

--- workspace/tim/stylish/Backend/Application/Features/Monitor/Queries/GetMonitor/GetMonitorHandler.js
+++ workspace/tim/stylish/Backend/Application/Features/Monitor/Queries/GetMonitor/GetMonitorHandler.js
@@ -12,24 +12,29 @@ module.exports = {
         //check
         let totalRevenue = 0;
         const checkDbforInsertOrNot = await monitorService.simpleSearch();
-        if(checkDbforInsertOrNot.length === 0){
+        if (checkDbforInsertOrNot.length === 0) {
             const responseArray = await tool.fetchOrder();
-            await monitorService.insertOrderList(res,responseArray);
+            const batchSize = 200;
+            for (let i = 0; i < dataArray.length; i += batchSize) {
+                const batch = responseArray.slice(i, i + batchSize);
+                await monitorService.insertOrderList(res, batch);
+            }
+            
         }
         totalRevenue = await monitorService.monitorRevenue(res);
 
 
         const colorMonitor = await monitorService.monitorByColor(res);
         console.log(colorMonitor);
-        if(colorMonitor.length == 0) return errorMsg.monitorProblem(res);
+        if (colorMonitor.length == 0) return errorMsg.monitorProblem(res);
         const colorRes = await tool.processColorData(colorMonitor);
 
         const priceRangeMonitor = await monitorService.monitorByPriceRange(res);
-        if(priceRangeMonitor.length == 0) return errorMsg.monitorProblem(res);
+        if (priceRangeMonitor.length == 0) return errorMsg.monitorProblem(res);
         console.log(priceRangeMonitor);
 
         const topProductSizeMonitor = await monitorService.monitorBySize(res)
-        if(topProductSizeMonitor.length == 0) return errorMsg.monitorProblem(res);
+        if (topProductSizeMonitor.length == 0) return errorMsg.monitorProblem(res);
         console.log(topProductSizeMonitor);
 
 
