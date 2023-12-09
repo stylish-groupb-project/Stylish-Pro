e3d5fe262c103165d10c2cea99ff519155bb63b6
Lu-weiting <runeeld23@gmail.com>
2023-12-09 20:55:17 +0800

    Fix: service prob

--- workspace/tim/stylish/frontend/src/App.js
+++ workspace/tim/stylish/frontend/src/App.js
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import Cookies from "js-cookie";
-import HomePageLayout from "./pages/homePage";
+// import HomePageLayout from "./pages/homePage";
 // import ProductDetailPageLayout from "./pages/productDetailPage";
 // import CheckoutPage from "./pages/checkoutPage";
 // import ThankyouPage from "./pages/ThankyouPage";
@@ -36,7 +36,7 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/admin/dashboard.html" element={<MonitorPage />} />
-              <Route path="/" element={<HomePageLayout endpoint="all" />} />
+              {/* <Route path="/" element={<HomePageLayout endpoint="all" />} />
 
               <Route
                 path="/women"
@@ -47,7 +47,7 @@ function App() {
               <Route
                 path="/accessories"
                 element={<HomePageLayout endpoint="accessories" />}
-              />
+              /> */}
               {/* <Route path="/search" element={<HomePageLayout endpoint="search" />} />
 
               <Route path="/products/:id" element={<ProductDetailPageLayout endpoint="search" />} />
