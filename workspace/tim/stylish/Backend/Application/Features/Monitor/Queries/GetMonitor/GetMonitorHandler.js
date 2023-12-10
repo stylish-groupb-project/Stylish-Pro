2c7f9d6d64b9659aa2244c1dcbdd4bfa1a62338e
Lu-weiting <runeeld23@gmail.com>
2023-12-10 09:51:59 +0800

    Fix: service prob

--- workspace/tim/stylish/Backend/Application/Features/Monitor/Queries/GetMonitor/GetMonitorHandler.js
+++ workspace/tim/stylish/Backend/Application/Features/Monitor/Queries/GetMonitor/GetMonitorHandler.js
@@ -16,7 +16,7 @@ module.exports = {
             const responseArray = await tool.fetchOrder();
             const batchSize = 200;
             for (let i = 0; i < responseArray.length; i += batchSize) {
-                console.log(responseArray.slice(i, i + batchSize));
+                // console.log(responseArray.slice(i, i + batchSize));
                 const batch = responseArray.slice(i, i + batchSize)
                 await monitorService.insertOrderList(res, batch);
             }
